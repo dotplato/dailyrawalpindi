f1d43e50f94f7392ac34df7daa1f32f4531e11d4
sadiaKamal <sadiakamal2468@gmail.com>
2025-09-28 01:14:53 +0500

    removed needless import

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
-import Link from "next/link";
-import Image from "next/image";
+import "./styles/globals.css";
 import Navbar from "@/components/Navbar";
 
 const geistSans = Geist({
