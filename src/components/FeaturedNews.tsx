a31799d0c8dd2dd7d39b12f329f73b8890d3687c
sadiaKamal <sadiakamal2468@gmail.com>
2025-09-28 01:19:43 +0500

    featuring news based on admin's choice, removed undefined choice

--- src/components/FeaturedNews.tsx
+++ src/components/FeaturedNews.tsx
@@ -15,25 +15,26 @@ interface FeaturedNewsProps {
 }
 
 export default function FeaturedNews({ articles }: FeaturedNewsProps) {
-    return (
-      <div>
-        <h2 className="font-bold text-lg border-b pb-2 mb-2">Featured News</h2>
-        <ul>
-          {articles.map((article) => {
-            const href = article.fields.slug ? `/article/${article.fields.slug}` : undefined;
-            const Title = (
-              <p className="text-sm hover:text-blue-600 cursor-pointer">
-                {article.fields.title}
-              </p>
-            );
-            return (
-              <li key={article.sys.id} className="mb-3">
-                {href ? <Link href={href}>{Title}</Link> : Title}
-              </li>
-            );
-          })}
-        </ul>
-      </div>
-    );
-  }
-  
+  return (
+    <div>
+      <h2 className="font-bold text-lg border-b pb-2 mb-2">Featured News</h2>
+      <ul>
+        {articles.map((article) => {
+          const href = article.fields.slug
+            ? `/article/${article.fields.slug}`
+            : undefined;
+          const Title = (
+            <p className="text-sm hover:text-blue-600 cursor-pointer">
+              {article.fields.title}
+            </p>
+          );
+          return (
+            <li key={article.sys.id} className="mb-3">
+              {href ? <Link href={href}>{Title}</Link> : Title}
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+}
